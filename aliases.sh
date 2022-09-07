6fffd08c61e16fb45fae810e8a4cac122bb03301
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2022-09-07 11:23:17 +0200

    Add cd alias

--- aliases.sh
+++ aliases.sh
@@ -40,10 +40,14 @@ alias ccccccc="cd ../../../../../.."
 alias cdt="cd ~/work/repo/tsfpga/tsfpga"
 alias cdm="cd ~/work/repo/tsfpga/hdl_modules"
 alias cdr="cd ~/work/repo/tsfpga/hdl_registers"
+
 alias cdv="cd ~/work/repo/vunit/vunit"
+
 alias cde="cd ~/work/repo/truestream/embedded"
 alias cdd="cd ~/work/repo/truestream/documents"
 alias cdb="cd ~/work/repo/truestream/management"
+
+alias cds="cd ~/work/repo/fpga-spelling/fpga-spelling"
 
 
 
