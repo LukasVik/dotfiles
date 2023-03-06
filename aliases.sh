31fa73592b24fd4c1596425691823e458a5fd3f9
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2023-03-06 08:49:02 +0100

    Split aliases into general and truestream specific

--- aliases.sh
+++ aliases.sh
@@ -37,25 +37,6 @@ alias ccccc="cd ../../../.."
 alias cccccc="cd ../../../../.."
 alias ccccccc="cd ../../../../../.."
 
-alias cdt="cd ~/work/repo/truestream/embedded/external/tsfpga/tsfpga"
-alias cdm="cd ~/work/repo/truestream/embedded/external/hdl_modules/hdl_modules"
-alias cdr="cd ~/work/repo/truestream/embedded/external/hdl_registers/hdl_registers"
-
-alias cdv="cd ~/work/repo/vunit/vunit"
-
-alias cde="cd ~/work/repo/truestream/embedded"
-alias cdd="cd ~/work/repo/truestream/documents"
-alias cdb="cd ~/work/repo/truestream/management"
-
-alias cds="cd ~/work/repo/fpga-spelling/fpga-spelling"
-
-
-
-#
-# Quick command for opening VSCode workspace
-#
-alias codee="code ~/work/repo/truestream/embedded/embedded.code-workspace"
-
 
 
 #
---
+++ truestream_aliases.sh
@@ -0,0 +1,31 @@
+# Aliases/settings that are specific for development environment at Truestream AB.
+
+#
+# Directories
+#
+alias cdt="cd ~/work/repo/truestream/embedded/external/tsfpga/tsfpga"
+alias cdm="cd ~/work/repo/truestream/embedded/external/hdl_modules/hdl_modules"
+alias cdr="cd ~/work/repo/truestream/embedded/external/hdl_registers/hdl_registers"
+
+alias cdv="cd ~/work/repo/vunit/vunit"
+
+alias cde="cd ~/work/repo/truestream/embedded"
+alias cdd="cd ~/work/repo/truestream/documents"
+alias cdb="cd ~/work/repo/truestream/management"
+alias cdw="cd ~/work/repo/truestream/truestream.se"
+
+alias cds="cd ~/work/repo/fpga-spelling/fpga-spelling"
+
+
+
+#
+# Quick command for opening VSCode workspace
+#
+alias codee="code ~/work/repo/truestream/embedded/embedded.code-workspace"
+
+
+
+#
+# Add LaTeX theme to path.
+#
+export TEXMFHOME=$TEXMFHOME:~/work/repo/truestream/documents/latex_resources/texroot
